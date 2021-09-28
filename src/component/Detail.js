9a3745e9896c2b9d3e8e84881e84ff405dca7107
elmtree-Ahn <clfclf3138@gmail.com>
2021-09-28 13:57:07 +0900

    쇼핑몰 상세페이지 라우터 구현

--- src/component/Detail.js
+++ src/component/Detail.js
@@ -1,20 +1,24 @@
-import { useHistory } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 
 export default function Detail(props) {
 
+  let { id } = useParams();
+  let findItem = props.shoes.find(function(item){
+    return item.id == id
+  });
   let history = useHistory();
 
   return(
     <div className="container">
       <div className="row">
         <div className="col-md-6">
-          <img src={"https://codingapple1.github.io/shop/shoes" + (1) + ".jpg"}  width="100%" />
+          <img src={"https://codingapple1.github.io/shop/shoes" + (findItem.id + 1) + ".jpg"}  width="100%" />
           
         </div>
         <div className="col-md-6 mt-4">
-          <h4 className="pt-5">{props.shoes[0].title}</h4>
-          <p>{props.shoes[0].content}</p>
-          <p>{props.shoes[0].price}</p>
+          <h4 className="pt-5">{findItem.title}</h4>
+          <p>{findItem.content}</p>
+          <p>{findItem.price}</p>
           <button className="btn btn-danger">주문하기</button> 
           <button className="btn btn-danger" onClick={() => {history.goBack()}}>뒤로가기</button>
         </div>
